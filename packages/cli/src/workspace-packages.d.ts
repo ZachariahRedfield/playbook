a0d136cc455d7654a96556d8fb21a29d6b219c03
Fawxzzy <115301471+ZachariahRedfield@users.noreply.github.com>
2026-03-05 13:39:32 -0500

    Update workspace-packages.d.ts

--- packages/cli/src/workspace-packages.d.ts
+++ packages/cli/src/workspace-packages.d.ts
@@ -1,18 +1,18 @@
 declare module "@zachariahredfield/playbook-core" {
-  export const analyze: (...args: any[]): Promise<any>;
-  export const formatAnalyzeCi: (...args: any[]): Promise<any>;
-  export const formatAnalyzeHuman: (...args: any[]): Promise<any>;
-  export const formatAnalyzeJson: (...args: any[]): Promise<any>;
-  export const verify: (...args: any[]): Promise<any>;
-  export const formatHuman: (...args: any[]): Promise<any>;
-  export const formatJson: (...args: any[]): Promise<any>;
+  export const analyze: (...args: any[]) => Promise<any>;
+  export const formatAnalyzeCi: (...args: any[]) => Promise<any>;
+  export const formatAnalyzeHuman: (...args: any[]) => Promise<any>;
+  export const formatAnalyzeJson: (...args: any[]) => Promise<any>;
+  export const verify: (...args: any[]) => Promise<any>;
+  export const formatHuman: (...args: any[]) => Promise<any>;
+  export const formatJson: (...args: any[]) => Promise<any>;
 }
 
 declare module "@zachariahredfield/playbook-node" {
-  export const createNodeContext: (...args: any[]): Promise<any>;
+  export const createNodeContext: (...args: any[]) => Promise<any>;
 }
 
 declare module "@zachariahredfield/playbook-engine" {
-  export const loadConfig: (...args: any[]): Promise<any>;
-  export const generateArchitectureDiagrams: (...args: any[]): Promise<any>;
+  export const loadConfig: (...args: any[]) => Promise<any>;
+  export const generateArchitectureDiagrams: (...args: any[]) => Promise<any>;
 }
